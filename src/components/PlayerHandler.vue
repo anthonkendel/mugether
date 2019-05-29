7a4a84a04657a9abc7a4c619f88adebf9ab677d7
Anthon Holmqvist <anthon.holmqvist@softhouse.se>
2019-05-29 16:32:30 +0200

    Update player state and selected playlist once per 1,5 second

--- src/components/PlayerHandler.vue
+++ src/components/PlayerHandler.vue
@@ -7,6 +7,8 @@ import { SpotifyService } from '@/services/SpotifyService';
 import Vue from 'vue';
 import { AuthenticationService } from '@/services/AuthenticationService';
 import { Actions } from '@/store/actions';
+import { mapState } from 'vuex';
+import { Playlist } from '@/services/SpotifyInterfaces';
 
 export default Vue.extend({
   name: 'PlayerHandler',
@@ -16,16 +18,43 @@ export default Vue.extend({
   computed: {
     isAuthorized: () => AuthenticationService.isAuthorized(),
     accessToken: () => AuthenticationService.getAccessToken(),
+    selectedPlaylist: {
+      get(): Playlist {
+        return this.$store.state.selectedPlaylist;
+      },
+      async set(value: Playlist): Promise<void> {
+        if (this.$store.state.selectedPlaylist !== value) {
+          await this.$store.dispatch(Actions.setState, {
+            selectedPlaylist: value,
+          });
+        }
+      },
+    },
+    playlists(): Playlist[] {
+      return this.$store.state.availablePlaylists;
+    },
   },
   methods: {
+    async updateState(): Promise<void> {
+      const player = await SpotifyService.getPlayer(this.accessToken);
+      const [, playlistId] = player.context.uri.split(':playlist:');
+
+      if (player) {
+        await this.$store.dispatch(Actions.setState, { player });
+      }
+
+      if (playlistId && this.selectedPlaylist.id !== playlistId) {
+        const seletedPlaylist = this.playlists.find(
+          (playlist) => playlist.id === playlistId,
+        );
+        if (seletedPlaylist) {
+          this.selectedPlaylist = seletedPlaylist;
+        }
+      }
+    },
     handlePlayer(): void {
       if (this.isAuthorized) {
-        setInterval(async () => {
-          const player = await SpotifyService.getPlayer(this.accessToken);
-          if (player) {
-            await this.$store.dispatch(Actions.setState, { player });
-          }
-        }, 1000);
+        setInterval(this.updateState, 1500);
       }
     },
   },
